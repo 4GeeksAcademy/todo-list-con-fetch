c03a99380a87296f847911bf8a90f2c7fef9e3aa
Jimena <jparatge@gmail.com>
2024-10-21 01:44:27 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,14 +4,16 @@ import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
-	let [listaDeTareas, setListaDeTareas] = useState(["Bañarse", "Lavar los platos", "Cocinar"])
+	let [listaDeTareas, setListaDeTareas] = useState([])
 	const [nuevaTarea, setNuevaTarea] = useState("")
 
 	return (
-		<div className="container mt-5">
-			<h1 className="text-center mt-5 text-secondary">To-Do's</h1>
-			<div className="mx-auto col-6" />
+		<div className="container mt-5 w-50 text-center">
+			<div className= "card">
+			<h1 className="text-center mt-3 text-secondary">To-Do's</h1>
+			<div className="mx-auto col-2" />
 			<input type="text" className="form-control" placeholder="¿Qué necesitas hacer?"
 				value={nuevaTarea} onChange={(evento) => {
 					setNuevaTarea(evento.target.value)
@@ -25,10 +27,10 @@ const Home = () => {
 				console.log (evento.key)
 			}}	
 			/>
-			<ul className="">
+			<ul className="list-group list-group-flush p-2 d-flex justify-content-center align-items-center">
 				{listaDeTareas.map((item, index) => {
 					return (
-						<li key={index}>
+						<li key={index} className= "list-group-item-sp w-25 rounded-pill bg-light text-center">
 							{item}	<i onClick= {()=> {
 								const aux = listaDeTareas.filter((_task, ind)=>{
 									return (ind != index)
@@ -42,10 +44,13 @@ const Home = () => {
 
 
 			</ul>
-
-			<span>
-				{listaDeTareas.length} items left
-			</span>
+				<span className= "text-primary">
+					{(listaDeTareas.length==0)? "No hay tareas, agregar una ":`Tienes ${listaDeTareas.length} tarea(s) pendiente(s)` }
+				</span>
+			{/* <span>
+				{listaDeTareas.length} items left 
+			</span>  */}
+			</div>
 		</div>
 	);
 };
